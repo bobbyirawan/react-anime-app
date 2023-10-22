c996dc2af60fdd48589c6ca9d258ee867e43e5af
bobby <bobbyirawan3011@yahoo.com>
2023-10-22 11:36:42 +0700

    initial commit anime-app

---
+++ src/App.jsx
@@ -0,0 +1,148 @@
+import { useState } from "react";
+import "./App.css";
+import { animesData } from "./data/Anime";
+
+export default function App() {
+  const [animes, setAnimes] = useState(animesData);
+  const [selectedAnime, setSelectedAnime] = useState(animes[0]);
+
+  function handleSelectedAnime(id) {
+    const newAnime = animes.filter((anime) => anime.mal_id === id);
+    setSelectedAnime(newAnime[0]);
+  }
+
+  return (
+    <>
+      <NavBar>
+        <Search>
+          <NumResult animes={animes} />
+        </Search>
+      </NavBar>
+      <Main>
+        <Box>
+          <AnimeList animes={animes} onSelectedAnime={handleSelectedAnime} />
+        </Box>
+        <Box>
+          <AnimeDetail selectedAnime={selectedAnime} />
+        </Box>
+      </Main>
+    </>
+  );
+}
+
+const NavBar = ({ children }) => {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      {children}
+    </nav>
+  );
+};
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <span role="img">🍥</span>
+      <h1>WeeBoo</h1>
+      <span role="img">🍥</span>
+    </div>
+  );
+};
+
+const Search = ({ children }) => {
+  const [query, setQuery] = useState("");
+
+  return (
+    <div className="search-container">
+      <input
+        className="search"
+        type="text"
+        placeholder="Search anime..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {children}
+    </div>
+  );
+};
+
+const NumResult = ({ animes }) => {
+  return (
+    <p className="search-results">
+      Found <strong>{animes.length}</strong> results
+    </p>
+  );
+};
+
+const Main = ({ children }) => {
+  return (
+    <main className="main">
+      {children}
+    </main>
+  );
+};
+
+const Box = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen((open) => !open)}
+      >
+        {isOpen ? "–" : "+"}
+      </button>
+      {isOpen &&  children }
+    </div>
+  );
+};
+
+const AnimeList = ({ animes, onSelectedAnime }) => {
+  return (
+    <ul className="list list-anime">
+      {animes?.map((anime) => (
+        <Anime
+          key={anime.mal_id}
+          anime={anime}
+          onSelectedAnime={onSelectedAnime}
+        />
+      ))}
+    </ul>
+  );
+};
+
+const Anime = ({ anime, onSelectedAnime }) => {
+  return (
+    <li onClick={() => onSelectedAnime(anime.mal_id)}>
+      <img src={anime.image} alt={`${anime.title} cover`} />
+      <h3>{anime.title}</h3>
+      <div>
+        <p>
+          <span>{anime.year}</span>
+        </p>
+      </div>
+    </li>
+  );
+};
+
+const AnimeDetail = ({ selectedAnime }) => {
+  return (
+    <div className="details">
+      <header>
+        <img src={selectedAnime.image} alt={`${selectedAnime.title} cover`} />
+        <div className="details-overview">
+          <h2>{selectedAnime.title}</h2>
+          <p>
+            {selectedAnime.year} &bull; {selectedAnime.score}
+          </p>
+        </div>
+      </header>
+      <section>
+        <p>
+          <em>{selectedAnime.synopsis}</em>
+        </p>
+      </section>
+    </div>
+  );
+};
